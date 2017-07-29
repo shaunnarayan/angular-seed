5b16b536a762eb694c01511fbbad6528dc59d04c
shaungit <shaun.narayan@neuritesoft.com>
2017-07-29 19:30:51 +1200

    Setup ngrx

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { Angulartics2Module, Angulartics2GoogleAnalytics } from "angulartics2";
 
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreRouterConnectingModule, routerReducer } from "@ngrx/router-store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+
 import { AboutModule } from "./about/about.module";
 import { HomeModule } from "./home/home.module";
 import { SharedModule } from "./shared/shared.module";
@@ -16,6 +21,13 @@ import { SharedModule } from "./shared/shared.module";
         HttpModule,
         AppRoutingModule,
         Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
+        StoreModule.forRoot({ app: {} }),
+        EffectsModule.forRoot([]),
+        StoreRouterConnectingModule,
+        // Note that you must instrument after importing StoreModule
+        StoreDevtoolsModule.instrument({
+            maxAge: 25 //  Retains last 25 states
+        }),
         AboutModule,
         HomeModule,
         SharedModule.forRoot()
