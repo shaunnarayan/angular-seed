366f4d497eac9547eb268681bbde3598c67666d0
shaungit <shaun.narayan@neuritesoft.com>
2017-07-29 19:20:01 +1200

    Add ngrx, tachyons, lodash, analytics Update lint conf

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,23 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { APP_BASE_HREF } from '@angular/common';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { APP_BASE_HREF } from "@angular/common";
+import { HttpModule } from "@angular/http";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { Angulartics2Module, Angulartics2GoogleAnalytics } from "angulartics2";
 
-import { AboutModule } from './about/about.module';
-import { HomeModule } from './home/home.module';
-import { SharedModule } from './shared/shared.module';
-
+import { AboutModule } from "./about/about.module";
+import { HomeModule } from "./home/home.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
-  declarations: [AppComponent],
-  providers: [{
-    provide: APP_BASE_HREF,
-    useValue: '<%= APP_BASE %>'
-  }],
-  bootstrap: [AppComponent]
-
+    imports: [
+        BrowserModule,
+        HttpModule,
+        AppRoutingModule,
+        Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
+        AboutModule,
+        HomeModule,
+        SharedModule.forRoot()
+    ],
+    declarations: [AppComponent],
+    providers: [
+        {
+            provide: APP_BASE_HREF,
+            useValue: "<%= APP_BASE %>"
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
